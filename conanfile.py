88ab20f476960abe817682947505d5ee4f7ff4a4
borune-k12 <borune.k12@gmail.com>
2020-02-28 16:32:04 +0300

    initial

---
+++ conanfile.py
@@ -0,0 +1,83 @@
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import os
+
+
+class EtcdGrpcConan(ConanFile):
+    name = "etcd-grpc"
+    version = "3.0.0"
+    description = "DSSL etcd gRPC headers library"
+    topics = ("conan", "grpc", "protobuf", "etcd")
+    url = "https://github.com/trassir/etcd-grpc"
+    homepage = "https://github.com/trassir/etcd-grpc"
+    generators = "cmake"
+
+    settings = "os", "arch", "compiler"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False]
+    }
+    default_options = {
+	"shared": True,
+        "fPIC": True
+    }
+
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+
+    requires = (
+        "c-ares/1.15.0@conan/stable",
+        "grpc/1.25.0@inexorgame/stable",
+        "protobuf/3.9.1@bincrafters/stable"	
+    )
+
+    def configure(self):
+        if self.settings.os == "Windows":
+            raise ConanInvalidConfiguration("etcd-grpc should not be built on Windows")
+       
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+        cmake_path = os.path.join(self._source_subfolder, "CMakeLists.txt")        
+
+    def _configure_cmake(self):
+        cmake = CMake(self)        
+
+        if self.options.shared:
+            cmake.definitions["BUILD_SHARED_LIBS"] = "ON"
+
+        cmake.configure(build_folder=self._build_subfolder, source_folder=self._source_subfolder)
+        return cmake
+
+    def build(self):
+        protobuf_module = self.deps_cpp_info["protobuf"]
+        protoc_path = os.path.join(protobuf_module.rootpath, protobuf_module.bindirs[0], 'protoc')
+        protobuf_include_dir = os.path.join(protobuf_module.rootpath, protobuf_module.include_paths[0])
+
+        grpc_module = self.deps_cpp_info["grpc"]
+        grpc_cpp_plugin_path = os.path.join(grpc_module.rootpath, grpc_module.bindirs[0], 'grpc_cpp_plugin')
+        grpc_include_dir = os.path.join(grpc_module.rootpath, grpc_module.include_paths[0])
+
+        proto_dir = "%s/proto" % self._source_subfolder
+        out_dir = "%s/src" % self._source_subfolder
+        
+        self.run("%s -I %s --grpc_out=%s --plugin=protoc-gen-grpc=%s %s/rpc.proto" %
+            (protoc_path, proto_dir, out_dir, grpc_cpp_plugin_path, proto_dir),
+            run_environment=True)
+
+        self.run("%s -I %s --cpp_out=%s %s/*.proto" %
+            (protoc_path, proto_dir, out_dir, proto_dir),
+            run_environment=True)
+
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs.append("etcd-grpc")
+        
